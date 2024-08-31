63826b457f24fe5456f5450ba454c2ddd6b4230a
BederCC <beder1810@gmail.com>
2024-08-30 22:06:50 -0500

    arreglos menores

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const db = require('./db')
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({extended:true}))
+
 app.use(express.json())
 
 app.use(express.static('public'))
--- db.js
+++ db.js
@@ -9,6 +9,6 @@ mongoose.connect(url, {
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'Error al conectar MongoDB'))
 db.once('open', function callback() {
-    console.log("¡Conectado a MongoDB!")
+    console.log("¡Conectado a MongoDB!!!")
 })
 module.exports = db
